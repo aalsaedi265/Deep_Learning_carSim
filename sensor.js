7884936bed9fe7a5342c34684a487f9466f23532
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-01-05 19:59:43 -0600

    senor

--- sensor.js
+++ sensor.js
@@ -1,74 +1,106 @@
+class Sensor {
+  constructor(car) {
+    this.car = car;
+    this.rayCount = 5;
+    this.rayLength = 150;
+    this.raySpread = Math.PI / 2;
 
-class Sensor{
-    constructor(car) {
-        this.car = car
-        this.rayCount = 7
-        this.rayLength = 70
-        this.raySpread = Math.PI /2 //45 degrees
-        
-        this.rays = []
-        this.readings = []
+    this.rays = [];
+    this.readings=[];
+  }
 
-    }
-
-    update(roadBorders) {
-        this.#castRays()
-        this.readings = []
-        for (let i = 0; i < this.rays.length; i++){
-            this.readings.push(
-                this.getReading(this.rays[i], roadBorders)
+  update(roadBorders) {
+    this.#castRays();
+    this.readings=[];
+    for(let i=0; i< this.rays.length; i++) {
+        this.readings.push(
+            this.#getReading(
+              this.rays[i],
+              roadBorders
+              
             )
-        }
+        );
     }
-    getReading(ray, roadBorders) {
-        let touches = []
-        for (let i = 0; i < roadBorders.length; i++){
-            const touch = getIntersection(
+  }
+
+  #getReading(ray,roadBorders) {
+    let touches=[];
+
+    for(let i=0;i<roadBorders.length;i++){
+        const touch=getIntersection(
             ray[0],
             ray[1],
-            roadBorders[i][0]
-            )
-            if (touch) {
-                touches.push(touch)
+            roadBorders[i][0],
+            roadBorders[i][1]
+        );
+        if(touch){
+            touches.push(touch);
+        }
+    }
 
-            }
-        }
-        if (touches.length == 0) {
-            return null
-        } else {
-            const offset = touches.map(e => e.offset)
-            const minOffset = min(...offset)
-            return touches.find(e => e.offset == minOffset)
-        }
+    if(touches.length==0){
+        // if no touching, no reading anything
+        return null;
+    }else {
+        const offsets=touches.map(e=>e.offset);
+        const minOffset=Math.min(...offsets); 
+        
+        return touches.find(e=>e.offset == minOffset);
+    }    
+  }
+
+  #castRays() {
+    this.rays = [];
+    for (let i = 0; i < this.rayCount; i++) {
+      const rayAngle =
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1) 
+        ) + this.car.angle;
+      const start = { x: this.car.x, y: this.car.y };
+      const end = {
+        x: this.car.x - Math.sin(rayAngle) * this.rayLength,
+        y: this.car.y - Math.cos(rayAngle) * this.rayLength,
+      };
+      this.rays.push([start, end]);
     }
-    #castRays() {
-        this.rays = []
-        for (let i = 0; i < this.rayCount; i++){
-            const rayAngle = lerp(
-                this.raySpread / 2,
-                -this.raySpread / 2,
-                this.rayCount ==1? 0.5 : i / (this.rayCount - 1)
-            ) + this.car.angle
-            const start = { x: this.car.x, y: this.car.y }
-            const end = {
-                x: this.car.x - Math.sin(rayAngle)*this.rayLength,
-                y: this.car.y - Math.cos(rayAngle)*this.rayLength
-            }
-            this.rays.push([start, end]) //putting a segment
-        }
-    }
+   }
+
     draw(ctx) {
-        if (this.rays.length === 0) {
-            return; // Do nothing if rays are not yet initialized
-        }
-            for (let i = 0; i < this.rayCount; i++){
-                ctx.beginPath()
-                ctx.lineWidth = 2
-                ctx.strokeStyle = "yellow"
-                ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y)
-                ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y)
-                ctx.stroke()
+        if (this.rays.length === 0) return
+        for (let i = 0; i < this.rayCount; i++) {
+            let end=this.rays[i][1];
+            if(this.readings[i]){
+                //if there is readings
+                end=this.readings[i]; //which calls getIntersection()
             }
+
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "yellow";
+            ctx.moveTo(
+                this.rays[i][0].x, 
+                this.rays[i][0].y
+            );
+            ctx.lineTo(
+                end.x, 
+                end.y
+            );
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "black";
+            ctx.moveTo(
+                this.rays[i][1].x, 
+                this.rays[i][1].y
+            );
+            ctx.lineTo(
+                end.x, 
+                end.y
+            );
+            ctx.stroke();
         }
-    
+    }
 }
